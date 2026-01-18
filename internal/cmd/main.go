4f8991edaa26118dee0dbc1953613bd65894d915
Asher Siddique <ashersiddique127@gmail.com>
2026-01-19 04:53:50 +0500

    Add a launch.json for debugging; Add scaffolding functions for loading env vars

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"fmt"
+	"throughput-tester-client/internal/config"
+)
+
+func main() {
+	config := config.Load()
+	fmt.Println(config.RuntimeConfigs.TestVar1)
+}
--- internal/config/loader.go
+++ internal/config/loader.go
@@ -1,1 +1,55 @@
 package config
+
+import (
+	"os"
+	"strconv"
+)
+
+// Runtime specific variables
+type runtimeConfigs struct {
+	TestVar1 int
+	TestVar2 string
+}
+
+// Generic struct that holds specific structs
+type Config struct {
+	RuntimeConfigs *runtimeConfigs
+	// Add more structs as needed
+}
+
+// Load configs from .env
+func Load() *Config {
+	return &Config{
+		RuntimeConfigs: loadRuntimeConfigs(),
+	}
+}
+
+// Load runtime configs from .env
+func loadRuntimeConfigs() *runtimeConfigs {
+	return &runtimeConfigs{
+		TestVar1: loadIntfromEnv("TEST_VAR_1", 999),
+		TestVar2: loadStrfromEnv("TEST_VAR_2", "some-value"),
+	}
+}
+
+// Returns the value of varName. If it's empty or not present, then
+// returns defVal
+func loadIntfromEnv(varName string, defVal int) int {
+	i, err := strconv.ParseInt(os.Getenv(varName), 10, 32)
+	if err != nil {
+		return defVal
+	} else {
+		return int(i)
+	}
+}
+
+// Returns the value of varName even if it's empty (if present).
+// Otherwise returns defVal
+func loadStrfromEnv(varName, defVal string) string {
+	val, present := os.LookupEnv(varName)
+	if present {
+		return val
+	} else {
+		return defVal
+	}
+}
